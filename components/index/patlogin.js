a059da349defcd71de3023ceb6ad7735bbcdb7a3
Shubham <shubhamsharma0040@gmailcom>
2021-03-07 17:02:27 +0530

    QR Code Login Working

--- components/index/patlogin.js
+++ components/index/patlogin.js
@@ -1,26 +1,58 @@
 import React, { Component } from "react";
 import { Button, Form, Input, Message } from "semantic-ui-react";
+import Modal from "react-modal";
+
+import dynamic from "next/dynamic";
+const QrReader = dynamic(() => import("react-qr-reader"), {
+  ssr: false,
+});
 
 import hospital from "../abis/Hospital";
 import web3 from "../abis/web3";
 import routes, { Router } from "../../routes";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 class Patlogin extends Component {
   state = {
     id: "",
     loading: false,
     errorMessage: "",
+    result: "No Result",
+    modal: false,
   };
 
-  onSubmit = async (event) => {
-    event.preventDefault();
+  handleScan = (data) => {
+    if (data) {
+      this.setState({
+        id: data,
+        result: data,
+      });
+      this.onSubmit();
+    }
+  };
+  handleError = (err) => {
+    console.error(err);
+  };
 
-    this.setState({ loading: true, errorMessage: "" });
+  onSubmit = async () => {
+    // event.preventDefault();
+
+    // this.setState({ loading: true, errorMessage: "" });
 
     try {
       const address = await hospital.methods.enterPatient(this.state.id).call();
       Router.pushRoute(`/patient/${address}`);
-    } catch (error) {
+    } catch (err) {
       this.setState({ errorMessage: err.message });
     }
 
@@ -28,11 +60,36 @@ class Patlogin extends Component {
   };
 
   render() {
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    };
+
     return (
       <div>
-        <h3>Enter a New Patient</h3>
+        <Modal style={customStyles} isOpen={this.state.modal}>
+          <QrReader
+            delay={300}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ previewStyle }}
+          />
+          <p>{this.state.result}</p>
+          <br></br>
+
+          <Button
+            loading={this.state.loading}
+            onClick={() => this.setState({ modal: false })}
+            type="submit"
+            negative
+          >
+            Close
+          </Button>
+        </Modal>
+
+        <h3>Login Patient</h3>
         <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
-          <Form.Field>
+          {/* <Form.Field>
             <label>Patient's ID</label>
             <Input
               type="password"
@@ -40,11 +97,16 @@ class Patlogin extends Component {
               value={this.state.id}
               onChange={(event) => this.setState({ id: event.target.value })}
             />
-          </Form.Field>
+          </Form.Field> */}
 
           <Message error header="Oops!!" content={this.state.errorMessage} />
 
-          <Button loading={this.state.loading} type="submit" primary>
+          <Button
+            loading={this.state.loading}
+            onClick={() => this.setState({ modal: true })}
+            type="submit"
+            primary
+          >
             Login a Patient
           </Button>
         </Form>
